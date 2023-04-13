da7b727c160a53117a6550f3caafa1eb7dcb0485
itsik <itsik.goldberg@gmail.com>
2023-04-13 18:22:22 +0300

    refactor code

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-// Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- public/index.js
+++ public/index.js
@@ -1,24 +1,17 @@
 const base64String ="JVBERi0xLjMNCiXi48/TDQoNCjEgMCBvYmoNCjw8DQovVHlwZSAvQ2F0YWxvZw0KL091dGxpbmVzIDIgMCBSDQovUGFnZXMgMyAwIFINCj4+DQplbmRvYmoNCg0KMiAwIG9iag0KPDwNCi9UeXBlIC9PdXRsaW5lcw0KL0NvdW50IDANCj4+DQplbmRvYmoNCg0KMyAwIG9iag0KPDwNCi9UeXBlIC9QYWdlcw0KL0NvdW50IDINCi9LaWRzIFsgNCAwIFIgNiAwIFIgXSANCj4+DQplbmRvYmoNCg0KNCAwIG9iag0KPDwNCi9UeXBlIC9QYWdlDQovUGFyZW50IDMgMCBSDQovUmVzb3VyY2VzIDw8DQovRm9udCA8PA0KL0YxIDkgMCBSIA0KPj4NCi9Qcm9jU2V0IDggMCBSDQo+Pg0KL01lZGlhQm94IFswIDAgNjEyLjAwMDAgNzkyLjAwMDBdDQovQ29udGVudHMgNSAwIFINCj4+DQplbmRvYmoNCg0KNSAwIG9iag0KPDwgL0xlbmd0aCAxMDc0ID4+DQpzdHJlYW0NCjIgSg0KQlQNCjAgMCAwIHJnDQovRjEgMDAyNyBUZg0KNTcuMzc1MCA3MjIuMjgwMCBUZA0KKCBBIFNpbXBsZSBQREYgRmlsZSApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY4OC42MDgwIFRkDQooIFRoaXMgaXMgYSBzbWFsbCBkZW1vbnN0cmF0aW9uIC5wZGYgZmlsZSAtICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjY0LjcwNDAgVGQNCigganVzdCBmb3IgdXNlIGluIHRoZSBWaXJ0dWFsIE1lY2hhbmljcyB0dXRvcmlhbHMuIE1vcmUgdGV4dC4gQW5kIG1vcmUgKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NTIuNzUyMCBUZA0KKCB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDYyOC44NDgwIFRkDQooIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjE2Ljg5NjAgVGQNCiggdGV4dC4gQW5kIG1vcmUgdGV4dC4gQm9yaW5nLCB6enp6ei4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNjA0Ljk0NDAgVGQNCiggbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDU5Mi45OTIwIFRkDQooIEFuZCBtb3JlIHRleHQuIEFuZCBtb3JlIHRleHQuICkgVGoNCkVUDQpCVA0KL0YxIDAwMTAgVGYNCjY5LjI1MDAgNTY5LjA4ODAgVGQNCiggQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA1NTcuMTM2MCBUZA0KKCB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBFdmVuIG1vcmUuIENvbnRpbnVlZCBvbiBwYWdlIDIgLi4uKSBUag0KRVQNCmVuZHN0cmVhbQ0KZW5kb2JqDQoNCjYgMCBvYmoNCjw8DQovVHlwZSAvUGFnZQ0KL1BhcmVudCAzIDAgUg0KL1Jlc291cmNlcyA8PA0KL0ZvbnQgPDwNCi9GMSA5IDAgUiANCj4+DQovUHJvY1NldCA4IDAgUg0KPj4NCi9NZWRpYUJveCBbMCAwIDYxMi4wMDAwIDc5Mi4wMDAwXQ0KL0NvbnRlbnRzIDcgMCBSDQo+Pg0KZW5kb2JqDQoNCjcgMCBvYmoNCjw8IC9MZW5ndGggNjc2ID4+DQpzdHJlYW0NCjIgSg0KQlQNCjAgMCAwIHJnDQovRjEgMDAyNyBUZg0KNTcuMzc1MCA3MjIuMjgwMCBUZA0KKCBTaW1wbGUgUERGIEZpbGUgMiApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY4OC42MDgwIFRkDQooIC4uLmNvbnRpbnVlZCBmcm9tIHBhZ2UgMS4gWWV0IG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NzYuNjU2MCBUZA0KKCBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSB0ZXh0LiBBbmQgbW9yZSApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY2NC43MDQwIFRkDQooIHRleHQuIE9oLCBob3cgYm9yaW5nIHR5cGluZyB0aGlzIHN0dWZmLiBCdXQgbm90IGFzIGJvcmluZyBhcyB3YXRjaGluZyApIFRqDQpFVA0KQlQNCi9GMSAwMDEwIFRmDQo2OS4yNTAwIDY1Mi43NTIwIFRkDQooIHBhaW50IGRyeS4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gQW5kIG1vcmUgdGV4dC4gKSBUag0KRVQNCkJUDQovRjEgMDAxMCBUZg0KNjkuMjUwMCA2NDAuODAwMCBUZA0KKCBCb3JpbmcuICBNb3JlLCBhIGxpdHRsZSBtb3JlIHRleHQuIFRoZSBlbmQsIGFuZCBqdXN0IGFzIHdlbGwuICkgVGoNCkVUDQplbmRzdHJlYW0NCmVuZG9iag0KDQo4IDAgb2JqDQpbL1BERiAvVGV4dF0NCmVuZG9iag0KDQo5IDAgb2JqDQo8PA0KL1R5cGUgL0ZvbnQNCi9TdWJ0eXBlIC9UeXBlMQ0KL05hbWUgL0YxDQovQmFzZUZvbnQgL0hlbHZldGljYQ0KL0VuY29kaW5nIC9XaW5BbnNpRW5jb2RpbmcNCj4+DQplbmRvYmoNCg0KMTAgMCBvYmoNCjw8DQovQ3JlYXRvciAoUmF2ZSBcKGh0dHA6Ly93d3cubmV2cm9uYS5jb20vcmF2ZVwpKQ0KL1Byb2R1Y2VyIChOZXZyb25hIERlc2lnbnMpDQovQ3JlYXRpb25EYXRlIChEOjIwMDYwMzAxMDcyODI2KQ0KPj4NCmVuZG9iag0KDQp4cmVmDQowIDExDQowMDAwMDAwMDAwIDY1NTM1IGYNCjAwMDAwMDAwMTkgMDAwMDAgbg0KMDAwMDAwMDA5MyAwMDAwMCBuDQowMDAwMDAwMTQ3IDAwMDAwIG4NCjAwMDAwMDAyMjIgMDAwMDAgbg0KMDAwMDAwMDM5MCAwMDAwMCBuDQowMDAwMDAxNTIyIDAwMDAwIG4NCjAwMDAwMDE2OTAgMDAwMDAgbg0KMDAwMDAwMjQyMyAwMDAwMCBuDQowMDAwMDAyNDU2IDAwMDAwIG4NCjAwMDAwMDI1NzQgMDAwMDAgbg0KDQp0cmFpbGVyDQo8PA0KL1NpemUgMTENCi9Sb290IDEgMCBSDQovSW5mbyAxMCAwIFINCj4+DQoNCnN0YXJ0eHJlZg0KMjcxNA0KJSVFT0YNCg=="
-const pdfjsLib = window['pdfjs-dist/build/pdf'];
-pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+const pdfjsLib = window['pdfjs-dist/build/pdf']
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js'
 
-const searchText = document.querySelector('#search-text');
-const displayText = document.querySelector('#display-text');
-const loadingTask = pdfjsLib.getDocument({data: atob(base64String) });
-
-
-
- const downloadFile = () => {
-  loadingTask.promise.then(pdf => {
+const searchText = document.querySelector('#search-text')
+const displayText = document.querySelector('#display-text')
+const loadingTask = pdfjsLib.getDocument({data: atob(base64String) })
 
 
+const downloadFile = async () => {
 
-    console.log(  pdf.JSON)
-    const numPages = pdf.numPages;
+    const pdf = await loadingTask.promise;
+    const numPages = await pdf.numPages;
     const pageTextPromises = [];
-   
-  //  const findText = pdf.findText('fff');
-//console.log('findText',findText);
 
     for (let i = 1; i <= numPages; i++) {
       const page = pdf.getPage(i);
@@ -26,30 +19,27 @@ const loadingTask = pdfjsLib.getDocument({data: atob(base64String) });
       pageTextPromises.push(pageTextPromise);
     }
     
-    const dateCreateFile = pdf.getMetadata().then(metadata => {
-      const creationDate = metadata.info.CreationDate;
-      const year = creationDate.substring(2, 6);
-      const month = parseInt(creationDate.substring(6, 8)) - 1;
-      const day = creationDate.substring(8, 10);
+    //get matadata (date create file)
+    const metadata = await pdf.getMetadata()
+    const creationDate = metadata.info.CreationDate;
+    const year = creationDate.substring(2, 6);
+    const month = parseInt(creationDate.substring(6, 8)) - 1;
+    const day = creationDate.substring(8, 10);
+    const dateCreate = `${day}-${month}-${year}`
 
-      const dateCreate = new Date(year, month, day);
-      
-      console.log(dateCreate.toLocaleString()); // Output: "3/1/2006,
-      return dateCreate
-    });
+    Promise.all(pageTextPromises,metadata).then(pageTexts => {
+      const fileText =  pageTexts.map(pageText => pageText.items.map(item => item.str));
+      const allFileText = fileText.join('\n');
+      const fileName = fileText[0][0]
 
-    Promise.all(pageTextPromises,dateCreateFile).then(pageTexts => {
-      const fullText = pageTexts.map(pageText => pageText.items.map(item => item.str)).join('\n');
-      const fileName = pageTexts.map(pageText => pageText.items.map(item => item.str))[0][0]
-      debugger
-      let data = {
+      const data = {
         "data": 
         [
           {
-            fullText, 
+            allFileText, 
             fileName,
             numPages,
-            dateCreateFile
+            dateCreate
           }
         ]
       }
@@ -66,51 +56,37 @@ const loadingTask = pdfjsLib.getDocument({data: atob(base64String) });
       link.href = URL.createObjectURL(file);
       link.download = fileName;
       link.click();
-
     });
-  });
 }
 
 
+searchText.addEventListener('input', async(e) => {
 
-
-
-searchText.addEventListener('input', (e) => {
-
-  const myDiv = document.getElementById('myDiv');
-  myDiv.innerHTML = '';
   const wordToSearch = e.target.value.toLowerCase();
-  const myContainer = document.getElementById("myDiv");
-
+  const displayText = document.getElementById("display-text");
+  displayText.innerHTML = '';
   
-  loadingTask.promise.then(function(pdf) {
-    const pageNumber = 1;
-    pdf.getPage(pageNumber).then(function(page) {
-    
-      for(let i = 1; i <= pdf.numPages; i++) {
-        pdf.getPage(i).then(page => {
-          page.getTextContent().then(textContent => {
-            pageText = textContent.items.map(item => item.str).join('').toLowerCase();
-           // console.log(`Page ${i}: ${pageText}`);
+  const pdf = await loadingTask.promise
 
-            const findWord = findWordContainingSubstring(pageText, wordToSearch)
+  for(let i = 1; i <= await pdf.numPages; i++) {
+    const page = await pdf.getPage(i)
+    const textContent = await page.getTextContent()
+    const pageText = textContent.items.map(item => item.str).join('').toLowerCase();
+    // console.log(`Page ${i}: ${pageText}`);
 
-            if (findWord != null) { //find..
-              const result = getWordsAroundSearchTerm(pageText, findWord);
-              let findText  =  `${result.replace(new RegExp(wordToSearch, 'gi'), '<b>$&</b>')}`
+    const findWord = findWordContainingSubstring(pageText, wordToSearch)
 
-              const newDiv = document.createElement("div");
-              newDiv.innerHTML = findText
-              
-              myContainer.appendChild(newDiv);
-            }
-          })
-        })
-      }
-    })
-  })  
-});
+    if (findWord != null) { //find..
+      const result = getWordsAroundSearchTerm(pageText, findWord);
+      let findText = `${result.replace(new RegExp(wordToSearch, 'gi'), '<b>$&</b>')}`
 
+      const resultElement = document.createElement("div");
+      resultElement.innerHTML = findText
+      
+      displayText.appendChild(resultElement);
+    }     
+  }
+})
 
 
 function findWordContainingSubstring(str, substr) {
@@ -119,14 +95,11 @@ function findWordContainingSubstring(str, substr) {
   return match ? match[0] : null;
 }
 
-
-function getWordsAroundSearchTerm(str, searchTerm, wordsBefore = 5, wordsAfter = 5) {
+function getWordsAroundSearchTerm(str, searchTerm, wordsBefore = 9, wordsAfter = 9) {
   const words = str.split(' ');
   const index = words.findIndex(word => word.includes(searchTerm));
 
-  if (index === -1) {
-    return [];
-  }
+  if (index === -1)  return [];
 
   const startIndex = Math.max(0, index - wordsBefore);
   const endIndex = Math.min(words.length, index + wordsAfter + 1);
@@ -134,4 +107,3 @@ function getWordsAroundSearchTerm(str, searchTerm, wordsBefore = 5, wordsAfter =
 
   return result.join(' ');
 }
-
